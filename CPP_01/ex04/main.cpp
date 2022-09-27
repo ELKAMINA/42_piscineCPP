6c8578190029d7b214d40ee6a4bc59885564ebb1
EL KHATTABI <elkhattabi.amina@gmail.com>
2022-09-28 00:39:50 +0200

    Ex04 pas encore fini

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -3,45 +3,82 @@
 
 // https://www.codespeedy.com/find-substring-from-text-file-in-cpp/
 
+std::string getting_filename(std::string file)
+{
+	int 			index 		= 0;
+	std::string		replacing	= "replace.txt";
+	int				i			= 0;
+	int				size		= 0;
+
+	size = file.size();
+	file.resize(size+9);
+	if (file.find(".") != std::string::npos)
+	{
+		index = file.find(".");
+		index++;
+		while (replacing[i])
+		{
+			file[index] = replacing[i];
+			index++;
+			i++;
+		}
+	}
+	return file;
+}
+
 int	main(int argc, char *arv[])
 {
 	if	(argc != 4)
 	{
 		std::cout << "Wrong number of arguments" << std::endl;
-		return ()
+		return 1;
 	}
 	else
 	{
 		std::ifstream	file;
-		std::ofstream	copy_file;
+		std::ofstream	file2;
+		std::string		newfile;
 		std::string		s1;
 		std::string		s2;
 		std::string		temp;
-		bool			present;
-		int				line;
 
 		s1 = arv[2];
 		s2 = arv[3];
 		file.open(arv[1]);
+		newfile = getting_filename(arv[1]);
+		std::cout << newfile << std::endl;
+		file2.open(newfile, std::ios::out);
 		if	(file.is_open())
 		{
-			std::string		input;
-			copy_file(arv[1] + ".replace");
-			copy_file.open()
-			while (getline(file, temp))
+			if	(file2.is_open())
 			{
-				
-				if	(temp.find(s1, std::string::npos)) // npos means until the end of the string.
+				std::cout << "arv3" << s2 << std::endl;
+				std::string		input;
+				while (std::getline(file, temp))
 				{
-					std::cout << "THE STRING IS HEEEWEE!" << std::endl;
-					ans = true;
+					std::cout << "TEMP : " << temp << std::endl;
+					if	(temp.find(s1) != std::string::npos) // npos means until the end of the string.
+					{
+						std::cout << "s1 : " << temp.find(s1) << std::endl;
+						std::cout << "THE STRING IS HEEEWEE!" << std::endl;
+						temp.clear();
+					}
+					else
+					{
+						file2 << temp;
+						file2 << std::endl;	
+					}
 				}
 			}
-
+			else
+			{
+				std::cout << "Could not open file2!" << std::endl;
+				file2.close();
+			}
 		}
 		else
 		{
-			std::cout << "Could not open the file !" << std::endl;
+			std::cout << "Could not open file !" << std::endl;
 			file.close();
 		}
 	}
