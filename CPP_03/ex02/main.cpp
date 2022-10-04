ffca00d4d0712697bfe4c2b2c5a676c95ce1a6a5
elkamina@yahoo.com <ael-khat@e1r5p6.clusters.42paris.fr>
2022-10-04 19:02:22 +0200

    WELLLLL debut ex04

--- CPP_03/ex02/main.cpp
+++ CPP_03/ex02/main.cpp
@@ -8,18 +8,15 @@
 
 int	main()
 {
-	ClapTrap joueur_un;
+	// ClapTrap joueur_un;
 	ScavTrap Kikou("HEY");
-	FragTrap wesh;
+	FragTrap wesh("SALUT");
 	Kikou.attack("Tito");
-	// // ClapTrap joueur_deux(joueur_un);
 	Kikou.guardGate();
-	// // joueur_un.attack("lolilol");
 	Kikou.attack("Garbage");
 	Kikou.takeDamage(2);
 	Kikou.beRepaired(5);
-	// // joueur_un.takeDamage(1000);
-	// // joueur_un.takeDamage(5);
+	wesh.attack("LOOOOL");
 	// // joueur_un.beRepaired(UINT32_MAX);
 	// joueur_un.beRepaired(10);
 	
